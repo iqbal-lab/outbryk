91b61611d8a106dc46e51e2efaa02fe8453738e4
Henk den Bakker <henkcdenbakker@gmail.com>
2015-09-13 12:57:45 -0500

    simplified vcf_CortextofastaSNPs.sh to be compatible with filter_cortex_vcf.pl and exclude SNP variant filter function

--- scripts/vcf_CortextofastaSNPs.sh
+++ scripts/vcf_CortextofastaSNPs.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 VCF=$1
 
-cat $VCF|grep 'PASS'|grep 'SVLEN=0;SVTYPE=SNP\s' > $VCF.passedSNPs.vcf;
-head -29 $VCF > header_vcf.txt
-cat header_vcf.txt $VCF.passedSNPs.vcf > $VCF.passedSNPsH.vcf
-cat $VCF.passedSNPsH.vcf | vcf-to-tab > snps.tab;
+cat $VCF | vcf-to-tab > snps.tab;
 vcf_tab_to_fasta_alignment.pl -i snps.tab > $VCF.all_snps.fasta;
-rm *.passedSNPs.vcf
-rm *.passedSNPsH.vcf
 rm snps.tab
-rm header_vcf.txt
