81f73b38cc21a54b9ea412509cccc7d448458412
rmnorris <rmnorris@well.ox.ac.uk>
2015-09-22 10:14:31 +0100

    add fasttree and handles changes in scripts it calls

--- scripts/analyse_clade.pl
+++ scripts/analyse_clade.pl
@@ -37,12 +37,13 @@ my $index_dir;
 my $ref_dir;
 my $phage_fasta;
 
-my $outbryk_dir = '/data2/users/rachel/projects/outbryk';
+my $outbryk_dir = '/Net/cycloid/data2/users/rachel/projects/outbryk';
 my $cortex_dir = '~/apps/cortex/';
 my $vcftools_dir = '~/apps/vcftools_0.1.13/';
 my $stampy_bin = '/apps/well/stampy/1.0.24-py2.7/stampy.py';
+my $fasttree_dir = '/apps/well/fasttree/20150325/FastTree';
 
-my $usage = "usage: $0 -i input_list.txt -o outdir -data_dir /path/to/samples/ -ref_mash /path/to/refs.msh -index_dir /path/to/full/INDEX -ref_dir /path/to/refs/";
+my $usage = "usage: $0 -i input_list.txt -o outdir -data_dir /path/to/samples/ -ref_mash /path/to/refs.msh -index_dir /path/to/full/INDEX -ref_dir /path/to/refs/ -phage_fasta /path/to/phages.fasta";
 
 my $result = GetOptions (	"i=s"			=> \$input_list,
 				"o=s"			=> \$outdir,
@@ -116,8 +117,10 @@ my $ret_run_calls = qx{$run_calls};
 #######################
 my $filter = "perl $outbryk_dir/scripts/filter_cortex_vcf.pl $outdir/results/vcfs/output_cluster_wk_flow_J_RefCO_FINALcombined_BC_calls_at_all_k.raw.vcf";
 my $ret_filter = qx{$filter};
-my $to_fasta = "bash $outbryk_dir/scripts/vcf_CortextofastaSNPs2.sh  $outdir/results/vcfs/output_cluster_wk_flow_J_RefCO_FINALcombined_BC_calls_at_all_k.raw.vcf.filtered";
+my $to_fasta = "bash $outbryk_dir/scripts/vcf_CortextofastaSNPs.sh  $outdir/results/vcfs/output_cluster_wk_flow_J_RefCO_FINALcombined_BC_calls_at_all_k.raw.vcf.filtered";
 my $ret_to_fasta = qx{$to_fasta};
+my $fasttree = "$fasttree_dir -nt $outdir/results/vcfs/output_cluster_wk_flow_J_RefCO_FINALcombined_BC_calls_at_all_k.raw.vcf.filtered.all_snps.fasta > $outdir/results/vcfs/output_cluster_wk_flow_J_RefCO_FINALcombined_BC_calls_at_all_k.raw.vcf.filtered.all_snps.fasta.fasttree";
+my $ret_fasttree = qx{$fasttree};
 my $snp_sites = "snp-sites -p -o $outdir/results/vcfs/mergedPassed.vcf.all_snps.phy $outdir/results/vcfs/output_cluster_wk_flow_J_RefCO_FINALcombined_BC_calls_at_all_k.raw.vcf.filtered.all_snps.fasta";
 my $ret_snp_sites = qx{$snp_sites};
 my $tree = "phyml -i $outdir/results/vcfs/mergedPassed.vcf.all_snps.phy -d nt -q -b 250 -m K80 --quiet";
